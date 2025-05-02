c08c8008d3f6c0ca1f4151d1208628ffd9e79b9e
basma <basmamounir57@gmail.com>
2025-05-02 21:45:58 +0300

    Setup basic project structure with dependencies

---
+++ src/main/java/org/example/jaxrsproductmanagement/ProductApp.java
@@ -0,0 +1,14 @@
+package org.example.jaxrsproductmanagement;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class ProductApp {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
